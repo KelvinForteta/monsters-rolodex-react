03be1218c8e168d306e23ea2c188f7eec3e92391
Kelvin Forteta <kelvin.forteta@gmail.com>
2019-11-05 23:36:40 +0100

    Updates

---
+++ precache-manifest.d1b71a83e604e6a2b04f7448daed40d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849b90defe0f3533081434cd64121dc8",
+    "url": "/monsters-rolodex-react/index.html"
+  },
+  {
+    "revision": "a2a836fe4a0b7e946452",
+    "url": "/monsters-rolodex-react/static/css/main.89e9abd8.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex-react/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "a2a836fe4a0b7e946452",
+    "url": "/monsters-rolodex-react/static/js/main.97173045.chunk.js"
+  },
+  {
+    "revision": "20da3bef917262aa1bb5",
+    "url": "/monsters-rolodex-react/static/js/runtime-main.0ba7919f.js"
+  }
+]);
